62472e6fe11f4af3513b0f3f0df3b6ad2864fddb
VENOMJ-P <jay63837@gmail.com>
2025-05-11 23:40:45 +0530

    add rate limiter

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,13 +1,27 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import rateLimit from "express-rate-limit";
 
 import appRoutes from "./routes/index.routes.js";
 import { PORT } from "./configs/server.config.js";
 import { connectDB } from "./configs/database.config.js";
-import {app,server} from "./configs/socket.config.js"
+import { app, server } from "./configs/socket.config.js";
 
 // const app = express();
+
+const limiter = rateLimit({
+  windowMs: 60 * 60 * 1000, // 60 minutes
+  max: 100, // Limit each IP to 100 requests per windowMs
+  message: {
+    status: "error",
+    message: "Too many requests, please try again later.",
+  },
+  standardHeaders: true, // Enables `RateLimit` headers
+  legacyHeaders: false, // Disables `X-RateLimit-*` headers
+});
+
+app.use(limiter);
 
 app.use(
   cors({
@@ -17,7 +31,7 @@ app.use(
 );
 
 app.use(express.json({ limit: "10mb" })); // Increase JSON payload limit
-app.use(express.urlencoded({ limit: "10mb", extended: true })); 
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(cookieParser());
 app.use(express.json());
 app.use("/api", appRoutes);
